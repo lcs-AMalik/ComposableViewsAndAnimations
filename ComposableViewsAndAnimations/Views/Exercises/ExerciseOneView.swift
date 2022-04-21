de9bc062c8e0615782a2580f53cbbf0a8e36fdad
Abdul <AMalik@lcs.on.ca>
2022-04-21 09:56:17 -0400

    Fully done exercise number 1

--- ComposableViewsAndAnimations/Views/Exercises/ExerciseOneView.swift
+++ ComposableViewsAndAnimations/Views/Exercises/ExerciseOneView.swift
@@ -20,6 +20,9 @@ struct ExerciseOneView: View {
 
     // Controls the hue of the circle
     @State private var hue: Color = .black
+    
+    // Controls the size of the circle
+    @State private var offset: CGFloat = -50.0
 
     // MARK: Computed properties
 
@@ -32,7 +35,9 @@ struct ExerciseOneView: View {
                 Circle()
                     .foregroundColor(hue)
                     .scaleEffect(scaleFactor)
+                    .offset(x: 0, y: offset)
                     .onTapGesture {
+                        
                         if scaleFactor > 0.2 {
                             // Reduce the size of the circle by a tenth
                             scaleFactor -= 0.1
@@ -44,7 +49,26 @@ struct ExerciseOneView: View {
                                         saturation: 0.8,
                                         brightness: 0.8)
                         }
+                        
+                        // Move the text down by whatever the value of "offset" is
+                        // This state change will be animated
+                        withAnimation(.interpolatingSpring(mass: 1.0, stiffness: 1.0, damping: 0.75, initialVelocity: 10)) {
+                            offset += 5
+                        }
                     }
+//                    .onTapGesture {
+//                        if scaleFactor > 0.2 {
+//                            // Reduce the size of the circle by a tenth
+//                            scaleFactor -= 0.1
+//                        } else {
+//                            // Make sure the button doesn't entirely disappear
+//                            scaleFactor = 1
+//                            // Change the color of the view to a random hue
+//                            hue = Color(hue: Double.random(in: 1...360) / 360.0,
+//                                        saturation: 0.8,
+//                                        brightness: 0.8)
+//                        }
+//                    }
                 
             }
             .navigationTitle("Exercise 1")
